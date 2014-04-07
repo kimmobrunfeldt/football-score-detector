9555231d29711cfc7f9b3a40198684da8b6eddab
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-04-07 11:25:36 +0300

    Improve quality

--- docs/algorithm/create-doc-material.sh
+++ docs/algorithm/create-doc-material.sh
@@ -6,7 +6,7 @@ rm *.jpg
 rm *.gif
 cp ../../debug/* .
 cp ../../testdata/real3.jpg testdata.jpg
-mogrify -geometry '500x>' *.jpg
+mogrify -quality 100 -geometry '500x>' *.jpg
 
 # Create straighten table animation
 mkdir giftemp
@@ -19,7 +19,7 @@ python burn-text.py giftemp/lower_long_side.jpg "4"
 python burn-text.py giftemp/large_straight.jpg "5"
 
 convert -quality 100 -delay 140 -loop 0 giftemp/large.jpg giftemp/found_blue.jpg giftemp/found_corners.jpg giftemp/lower_long_side.jpg giftemp/large_straight.jpg straighten_table.gif
-convert straighten_table.gif \( -clone 0 -set delay 250 \) -swap 0 +delete \( +clone -set delay 250 \) +swap +delete straighten_table.gif
+convert -quality 100 straighten_table.gif \( -clone 0 -set delay 250 \) -swap 0 +delete \( +clone -set delay 250 \) +swap +delete straighten_table.gif
 
 rm -r giftemp
 
@@ -28,7 +28,7 @@ mkdir giftemp
 cp corner_a.jpg corner_b.jpg corner_c.jpg corner_d.jpg corner_labels.jpg giftemp
 
 convert -quality 100 -delay 140 -loop 0 corner_a.jpg corner_b.jpg corner_c.jpg corner_d.jpg corner_labels.jpg label_corners.gif
-convert label_corners.gif \( -clone 0 -set delay 250 \) -swap 0 +delete \( +clone -set delay 250 \) +swap +delete label_corners.gif
+convert -quality 100 label_corners.gif \( -clone 0 -set delay 250 \) -swap 0 +delete \( +clone -set delay 250 \) +swap +delete label_corners.gif
 
 rm -r giftemp
 
@@ -37,6 +37,6 @@ mkdir giftemp
 cp table_ends.jpg table_middle.jpg table_middles_of_middle.jpg table_middles_of_middle_add.jpg table_score_box.jpg giftemp
 
 convert -quality 100 -delay 140 -loop 0 table_ends.jpg table_middle.jpg table_middles_of_middle.jpg table_middles_of_middle_add.jpg table_score_box.jpg find_score_blocks.gif
-convert find_score_blocks.gif \( -clone 0 -set delay 250 \) -swap 0 +delete \( +clone -set delay 250 \) +swap +delete find_score_blocks.gif
+convert -quality 100 find_score_blocks.gif \( -clone 0 -set delay 250 \) -swap 0 +delete \( +clone -set delay 250 \) +swap +delete find_score_blocks.gif
 
 rm -r giftemp
